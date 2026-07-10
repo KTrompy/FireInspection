5718f57ae774607ffb4b885286854f6b206b493b
KTrompy <kyletrompeter0@gmail.com>
2026-07-10 10:07:21 +0200

    Redesign: navy/gold blueprint theme, dark services grid, dedicated booking page

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import Logo from './Logo'
 
 const FOOTER_LINKS = [
@@ -24,18 +25,12 @@ export default function Footer() {
 
           <div className="flex justify-center">
             <div className="w-40 h-40 rounded-xl bg-white/5 border border-white/10 flex items-center justify-center">
-              <svg width="80" height="80" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M20 3 L34 8 V19 C34 27.5 28.5 33.8 20 37 C11.5 33.8 6 27.5 6 19 V8 Z"
-                  fill="rgba(255,255,255,0.06)"
-                  stroke="#c8322c"
-                  strokeWidth="1.6"
-                />
-                <path
-                  d="M20 11c1.6 2 2.6 3.4 2.6 5.2 0 1.1-.6 2-1.5 2.4 1.7-.2 3-1.6 3-3.4 0-1.1-.4-2-.9-2.8 2.3 1.6 3.8 4.3 3.8 7.1 0 3.7-3 6.7-6.9 6.7s-6.9-3-6.9-6.7c0-2.9 1.6-5.5 3.9-7.1-.4.7-.6 1.4-.6 2.2 0 1 .4 1.9 1.1 2.5-.4-.5-.6-1.1-.6-1.8 0-1.7 1.2-3.1 3-4.3z"
-                  fill="#b8934c"
-                />
-              </svg>
+              <Image
+                src="/the1.png"
+                alt="Trojan Integrated Solutions Logo"
+                width={150}
+                height={150}
+              />
             </div>
           </div>
 
